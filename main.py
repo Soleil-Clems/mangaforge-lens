c65c7fa26b698bc3095f13f828aed646eb96dd36
Soleil-Clems <ouisolclems@gmail.com>
2026-02-17 23:59:07 +0100

    Feat: detected text lang

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from mangaforge_lens.detector import MangaDetector
+from mangaforge_lens.ocr import MangaOCR
 
-detector = MangaDetector("tests/test_scan.jpg")
+scan_path="tests/test_scan.jpg"
+detector = MangaDetector(scan_path)
 zones = detector.detect_text_zones()
 
 print(f"{len(zones)} zones détectées")
 for zone in zones:
     print(zone)
 
-detector.preview(zones)
+#detector.preview(zones)
+
+ocr = MangaOCR(scan_path)
+print("Ocr denut")
+for zone in zones:
+    result = ocr.read_text(zone)
+    print(result)
---
+++ mangaforge_lens/ocr.py
@@ -0,0 +1,38 @@
+import pytesseract
+from manga_ocr import MangaOcr
+from langdetect import detect
+from PIL import Image
+import cv2 as cv
+
+
+class MangaOCR:
+    def __init__(self, image_path: str):
+        self.image = cv.imread(image_path)
+        self.manga_ocr = MangaOcr()
+
+    def extract_zone(self, zone: dict):
+        x, y, w, h = zone["x"], zone["y"], zone["w"], zone["h"]
+        cropped = self.image[y:y + h, x:x + w]
+        return cropped
+
+    def read_text(self, zone: dict):
+        cropped = self.extract_zone(zone)
+        pil_image = Image.fromarray(cv.cvtColor(cropped, cv.COLOR_BGR2RGB))
+
+        # On essaie d'abord avec pytesseract
+        text = pytesseract.image_to_string(pil_image).strip()
+
+        # Si rien trouvé on essaie avec manga_ocr (japonais)
+        if not text:
+            text = self.manga_ocr(pil_image)
+
+        # Détection de la langue
+        try:
+            language = detect(text)
+        except:
+            language = "unknown"
+
+        return {
+            "text": text,
+            "language": language
+        }
